091d00724f1804da1e3cbfa88c4f432ab060c424
zhehaowang <wangzhehao410305@gmail.com>
2017-01-04 17:02:37 -0800

    ros2ndn_converter: ndn-iot framework adaptation

--- publisher/src/ros2ndn_converter/src/ros2ndn_converter.cpp
+++ publisher/src/ros2ndn_converter/src/ros2ndn_converter.cpp
@@ -16,51 +16,91 @@ typedef unsigned long uint32;
 
 bool initializationPhase = true;
 
-ndn::NdnController::Parameters ncParams;
-JsonParameters jsonParameters;
-ros::NodeHandle nh("~");
+ndn::ptr_lib::shared_ptr<ndn::Face> face;
+ndn::ptr_lib::shared_ptr<ndn::KeyChain> keyChain;
+ndn::Name certName;
+
+int argc;
+char **argv;
 
 void startPublishing() {
-  ndn::NdnController ndnController(ncParams);
-  initializationPhase = false;
+  // initialization:
+  ros::init(argc, argv, "ros2ndn_converter");
+  
+  JsonParameters jsonParameters;
+  ros::NodeHandle nh("~");
 
-  if (0 == ndnController.startNdnProcessing())
-  { 
-    // setting up Active Tracks structure
-    ndn::opt::ActiveTracks::Parameters atParams;
-    atParams.jsonParameters = jsonParameters;
+  // declare instance start 
+  ndn::NdnController::instanceStart();
 
-    ndn::opt::ActiveTracks activeTracks(atParams);
+  // read Json parameters from node parameters file
+  nh.param("json/indent_size", jsonParameters.jsonIndentSize, 0);
+  nh.param("json/newline", jsonParameters.jsonNewline, false);
+  nh.param("json/spacing", jsonParameters.jsonSpacing, false);
+  nh.param("json/use_tabs", jsonParameters.jsonUseTabs, false);
 
-    // setting up Track Hint Publisher
-    ndn::opt::TrackHintPublisher::Parameters thpParams;
-    nh.param("hints/rate", thpParams.rate, 5);
-    nh.param("hints/freshness_period", thpParams.freshnessPeriod, 200);
-    thpParams.nh = nh;
-    thpParams.ndnController = &ndnController;
-    thpParams.activeTracks = &activeTracks;
-    
-    ndn::opt::TrackHintPublisher trackHintPublisher(thpParams);
+  try 
+  {
+    ndn::NdnController::Parameters ncParams;
+    // setting up NdnController
+    ncParams.nh = nh;
+    ncParams.face.reset(face.get());
+    ncParams.keyChain.reset(keyChain.get());
+    ncParams.certName = certName;
 
-    // setting up Track Publisher
-    ndn::opt::TrackPublisher::Parameters  tpParams;
-    nh.param("ndn/freshness_period", tpParams.freshnessPeriod, 5000);
-    tpParams.bufferLength = 2048;
-    tpParams.nh = ncParams.nh;
-    tpParams.jsonParameters = jsonParameters;
-    tpParams.activeTracks = &activeTracks;
-    tpParams.ndnController = &ndnController; 
+    std::string prefix, node_name;
+  
+    nh.param("ndn/prefix", prefix, std::string("/ndn/edu/ucla/remap"));
+    nh.param("ndn/node_name", node_name, std::string("node0"));
+    ncParams.prefix = ndn::NdnController::getInstancePrefix(prefix, node_name);
+    nh.param("ndn/segment_length", ncParams.segmentLength, 2048);
 
-    ndn::opt::TrackPublisher trackPublisher(tpParams);
+    ndn::NdnController ndnController(ncParams);
+    initializationPhase = false;
 
-    trackPublisher.subscribe("input_topic");
-    trackHintPublisher.start();
-    
-    ros::spin();
+    if (0 == ndnController.startNdnProcessing())
+    { 
+      // setting up Active Tracks structure
+      ndn::opt::ActiveTracks::Parameters atParams;
+      atParams.jsonParameters = jsonParameters;
 
-    trackHintPublisher.stop();
-    trackPublisher.unsubscribe();
-    ndnController.stopNdnProcessing();      
+      ndn::opt::ActiveTracks activeTracks(atParams);
+
+      // setting up Track Hint Publisher
+      ndn::opt::TrackHintPublisher::Parameters thpParams;
+      nh.param("hints/rate", thpParams.rate, 5);
+      nh.param("hints/freshness_period", thpParams.freshnessPeriod, 200);
+      thpParams.nh = nh;
+      thpParams.ndnController = &ndnController;
+      thpParams.activeTracks = &activeTracks;
+      
+      ndn::opt::TrackHintPublisher trackHintPublisher(thpParams);
+
+      // setting up Track Publisher
+      ndn::opt::TrackPublisher::Parameters  tpParams;
+      nh.param("ndn/freshness_period", tpParams.freshnessPeriod, 5000);
+      tpParams.bufferLength = 2048;
+      tpParams.nh = ncParams.nh;
+      tpParams.jsonParameters = jsonParameters;
+      tpParams.activeTracks = &activeTracks;
+      tpParams.ndnController = &ndnController; 
+
+      ndn::opt::TrackPublisher trackPublisher(tpParams);
+
+      trackPublisher.subscribe("input_topic");
+      trackHintPublisher.start();
+
+      ros::spin();
+
+      trackHintPublisher.stop();
+      trackPublisher.unsubscribe();
+      ndnController.stopNdnProcessing();      
+
+    }
+  }
+  catch (std::exception& exception) 
+  {
+      ROS_ERROR_STREAM("got exception: " << exception.what());
   }
 }
 
@@ -76,57 +116,28 @@ void onRequestFailed(std::string msg) {
 
 int
 main(int argc, char **argv)
-{
-  // initialization:
-  ros::init(argc, argv, "ros2ndn_converter");
+{    
+  ::argc = argc;
+  ::argv = argv;
 
-  // declare instance start 
-  ndn::NdnController::instanceStart();
+  std::string hostName = "localhost";
+  int portNum = 6363;
 
-  // read Json parameters from node parameters file
-  nh.param("json/indent_size", jsonParameters.jsonIndentSize, 0);
-  nh.param("json/newline", jsonParameters.jsonNewline, false);
-  nh.param("json/spacing", jsonParameters.jsonSpacing, false);
-  nh.param("json/use_tabs", jsonParameters.jsonUseTabs, false);
-
-  try 
-  {
-    // setting up NdnController
-    ncParams.nh = nh;
-
-    std::string prefix, node_name;
+  face.reset(new ndn::Face(hostName.c_str(), portNum));
+  ndn_iot::Bootstrap bootstrap(*face);
+  ndn::KeyChain defaultKeyChain;
+  keyChain.reset(bootstrap.setupDefaultIdentityAndRoot(ndn::Name(defaultKeyChain.getDefaultIdentity())).get());
+  certName = bootstrap.getDefaultCertificateName();
   
-    nh.param("ndn/prefix", prefix, std::string("/ndn/edu/ucla/remap"));
-    nh.param("ndn/node_name", node_name, std::string("node0"));
-    ncParams.prefix = ndn::NdnController::getInstancePrefix(prefix, node_name);
-    nh.param("ndn/segment_length", ncParams.segmentLength, 2048);
+  std::cout << "default certificate name is: " << certName.toUri() << std::endl;
 
-    std::string hostName;
-    int portNum;
-
-    nh.param("ndn/daemon_host", hostName, std::string("localhost"));
-    nh.param("ndn/deamon_port", portNum, 6363);
-
-    ncParams.face.reset(new ndn::Face(hostName.c_str(), portNum));
-    ndn_iot::Bootstrap bootstrap(*(ncParams.face.get()));
-    ncParams.keyChain.reset(bootstrap.setupDefaultIdentityAndRoot(ndn::Name(prefix)).get());
-    ndn::Name defaultCertificateName = bootstrap.getDefaultCertificateName();
-    
-    std::cout << "default certificate name is: " << defaultCertificateName.toUri() << std::endl;
-
-    bootstrap.requestProducerAuthorization(ndn::Name(prefix), "flow",
-      onRequestSuccess, 
-      onRequestFailed);
-  }
-  catch (std::exception& exception) 
-  {
-      ROS_ERROR_STREAM("got exception: " << exception.what());
-  }
+  bootstrap.requestProducerAuthorization(ndn::Name(std::string("/ndn/edu/ucla/remap")), "flow",
+    onRequestSuccess, 
+    onRequestFailed);
 
   while (initializationPhase) {
-    ncParams.face->processEvents();
+    face->processEvents();
     usleep(10000);
   }
-
   return 0;
 }
